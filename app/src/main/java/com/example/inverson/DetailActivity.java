2957cd1cd127a54dc097d7ab990b0f7e296ea802
Vinh <nguyenvinh1997@yahoo.de>
2019-04-28 16:59:46 +0200

    11.04

--- app/src/main/java/com/example/inverson/DetailActivity.java
+++ app/src/main/java/com/example/inverson/DetailActivity.java
@@ -1,14 +1,24 @@
 package com.example.inverson;
 
-import android.app.ActionBar;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    public ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +26,9 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageButton imgB = findViewById(R.id.imageButton);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,4 +40,30 @@ public class DetailActivity extends AppCompatActivity {
         });
     }
 
+    public void requestCameraPermission(){
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                    MY_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        requestCameraPermission();
+
+        Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(imageIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
 }
