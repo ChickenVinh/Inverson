9627ff635271120aeded691dec73856daaf12918
Vinh <nguyenvinh1997@yahoo.de>
2019-05-29 19:40:06 +0200

    Merge branch 'master' of /Users/vinhletruongnguyen/StudioProjects/Iverson with conflicts.

---
+++ app/src/main/java/com/mahc/custombottomsheet/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.mahc.custombottomsheet;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        // close splash activity
+        finish();
+    }
+}
